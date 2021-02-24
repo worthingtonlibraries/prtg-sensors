f4baab2d8098294d0d46c7b7e44bf51484032693
Worthington Libraries Tech Staff <16614694+wpltechstaff@users.noreply.github.com>
2021-02-24 14:15:04 -0500

    Statuspage.io
    Sensor to check the status of any statuspage.io page and create channels for each object. Just need to pass in the ID. 
    
    Some sample IDs:
    
    Wasabi 2r1tdgqr8ld5
    Bridge tcylbbyggms8
    Zoom 14qjgk812kgk
    AppRiver zfhpg8k0c3wq
    Cloudflare yh6f0r4529hb

---
+++ check-statuspage.ps1
@@ -0,0 +1,36 @@
+﻿param (
+$statuspage
+)
+
+$text = ((curl https://$statuspage.statuspage.io/api/v2/status.json -UseBasicParsing).Content | ConvertFrom-Json).status | Select-Object -ExpandProperty description
+
+$components = ((curl https://$statuspage.statuspage.io/api/v2/components.json -UseBasicParsing).Content | ConvertFrom-Json).components
+
+Write-Output "<prtg>"
+
+foreach ($component in $components) {
+
+switch ($($component.status)) {
+"operational" {$statusvalue = 0}
+"partial_outage" {$statusvalue = 1} 
+"major_outage" {$statusvalue = 2}
+"degraded_performance" {$statusvalue = 3}
+"under_maintenance" {$statusvalue = 4}
+default {$statusvalue = 5}
+}
+
+write-output @"
+<result>
+	<channel>$($component.name)</channel>
+	<value>$statusvalue</value>
+</result>
+
+"@
+}
+
+Write-Output @"
+
+<text>$text</text>
+</prtg>
+
+"@
