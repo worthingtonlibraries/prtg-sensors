e6e06f090ec1dde904ef9a9799ed19bb084ea838
Worthington Libraries Tech Staff <16614694+wpltechstaff@users.noreply.github.com>
2021-02-24 15:08:06 -0500

    SNMP Windows Process
    Sensor to check if a given process is running in Windows, using SNMP compared to the built-in sensor that uses WMI.

---
+++ check-snmpprocess.ps1
@@ -0,0 +1,18 @@
+﻿param (
+$IP = "",
+$OID = "1.3.6.1.2.1.25.4.2.1.2",
+$Community = "",
+$process = ""
+)
+
+if (!(Get-Module snmp)) {Install-Module snmp -force}
+
+$snmp = Invoke-SnmpWalk -IP $IP -OIDStart $OID -Community $Community
+
+ if ($snmp.data -contains $process) {
+	write-host "1:OK" 
+ 	exit(0)
+ } else {
+	write-host "0:Error" 
+	exit(4)
+ }  
